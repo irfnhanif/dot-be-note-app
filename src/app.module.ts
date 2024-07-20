9e7e32bc7cb651d32bd2e1a0a20ab1189c816bec
irfnhanif <14.irfanh@gmail.com>
2024-07-20 11:12:23 +0700

    feat: Add NotesController and NotesService to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { NotesController } from './notes/notes.controller';
+import { NotesService } from './notes/notes.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, NotesController],
+  providers: [AppService, NotesService],
 })
 export class AppModule {}
